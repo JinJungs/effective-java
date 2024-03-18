3970f1686423bbbaffc1b5f236619088854db4ba
JinJungs <nehciyz@gmail.com>
2024-03-18 19:38:12 +0900

    [item5] 완벽공략

--- src/main/java/me/whiteship/chapter01/item05/dependencyinjection/SpellChecker.java
+++ src/main/java/me/whiteship/chapter01/item05/dependencyinjection/SpellChecker.java
@@ -3,6 +3,7 @@ package me.whiteship.chapter01.item05.dependencyinjection;
 import me.whiteship.chapter01.item05.Dictionary;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 public class SpellChecker {
 
@@ -10,8 +11,11 @@ public class SpellChecker {
     // 전제가 하나 더 있다면 Dictionary 가 인터페이스여야 한다.
     private final Dictionary dictionary;
 
-    public SpellChecker(Dictionary dictionary) {
-        this.dictionary = dictionary;
+    // 직접 Dictionary를 주입받는 대신에 어떤 Dictionary를 리턴할 것인지 결정하는 Factory를 주입받아 한번 더 감싼다
+    // DictionaryFactory는 Supplier<Dictionary> 로 대체가능
+    // Supplier : 매개변수는 없고 리턴값은 있는 함수형 인터페이스
+    public SpellChecker(Supplier<? extends Dictionary> dictionaryFactory) {
+        this.dictionary = dictionaryFactory.get();
     }
 
     public boolean isValid(String word) {
--- src/test/java/me/whiteship/chapter01/item05/dependencyinjection/SpellCheckerTest.java
+++ src/test/java/me/whiteship/chapter01/item05/dependencyinjection/SpellCheckerTest.java
@@ -6,6 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SpellCheckerTest {
     void isValid() {
-        SpellChecker spellChecker = new SpellChecker(new DefaultDictionary()); // 테스트 시 MockDictionary를 생성하여 주입 가능.
+//        SpellChecker spellChecker = new SpellChecker(new DefaultDictionary()); // 테스트 시 MockDictionary를 생성하여 주입 가능.
+
+        // Suppplier<Dictionary>
+        SpellChecker spellChecker = new SpellChecker(DefaultDictionary::new);
+
+        // DictionaryFactory
+        SpellChecker spellChecker2 = new SpellChecker(DictionaryFactory::get);
+
+
     }
 }
